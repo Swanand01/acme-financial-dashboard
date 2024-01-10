0da2f274a4cf6644be5f37db3960cf4c6c8c5173
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-10 14:50:35 +0530

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })

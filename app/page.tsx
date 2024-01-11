b213e660bffe09d17c73ae46207d0560f4a06895
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-11 17:13:09 +0530

    Add signup link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,12 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <Link
+            href="/signup"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Sign up</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image

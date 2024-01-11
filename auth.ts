6932f3b49f940209ad8ea5da660ff2e666fb5cc6
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-11 16:55:36 +0530

    Add auth config and middleware

---
+++ auth.ts
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import { getUser } from './app/lib/data';
+import bcrypt from 'bcrypt';
+
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordsMatch) return user;
+                }
+                return null;
+            },
+        }),],
+});

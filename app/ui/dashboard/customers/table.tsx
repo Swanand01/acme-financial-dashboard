002c4181b17df5dba6bab82bfde4e200b72a1aa6
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-17 00:03:32 +0530

    Refactor CustomersTable component

--- app/ui/dashboard/customers/table.tsx
+++ app/ui/dashboard/customers/table.tsx
@@ -1,22 +1,18 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/customers/data';
+import { DeleteCustomerButton, UpdateCustomerButton } from './buttons';
 
 export default async function CustomersTable({
-  customers,
+  query,
+  currentPage,
 }: {
-  customers: FormattedCustomersTable[];
+  query: string;
+  currentPage: number;
 }) {
+  const customers = await fetchFilteredCustomers(query, currentPage);
+
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
-      <Search placeholder="Search customers..." />
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
@@ -55,6 +51,10 @@ export default async function CustomersTable({
                         <p className="text-xs">Paid</p>
                         <p className="font-medium">{customer.total_paid}</p>
                       </div>
+                      <div className="flex justify-end gap-2">
+                        <UpdateCustomerButton id={customer.id} />
+                        <DeleteCustomerButton id={customer.id} />
+                      </div>
                     </div>
                     <div className="pt-4 text-sm">
                       <p>{customer.total_invoices} invoices</p>
@@ -84,7 +84,7 @@ export default async function CustomersTable({
                 </thead>
 
                 <tbody className="divide-y divide-gray-200 text-gray-900">
-                  {customers.map((customer) => (
+                  {customers?.map((customer) => (
                     <tr key={customer.id} className="group">
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
@@ -109,6 +109,12 @@ export default async function CustomersTable({
                       </td>
                       <td className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
                         {customer.total_paid}
+                      </td>
+                      <td className="whitespace-nowrap bg-white py-3 pl-6 pr-3">
+                        <div className="flex justify-end gap-3">
+                          <UpdateCustomerButton id={customer.id} />
+                          <DeleteCustomerButton id={customer.id} />
+                        </div>
                       </td>
                     </tr>
                   ))}

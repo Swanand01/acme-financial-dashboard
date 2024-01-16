d50723ff2a070e33ece731549398f5a15ffe77d5
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-16 23:57:57 +0530

    Update imports in Create Invoice Page component and refactor Form

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,6 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
+import InvoiceForm from '@/app/ui/dashboard/invoices/form';
+import Breadcrumbs from '@/app/ui/dashboard/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/customers/data';
 
 export default async function Page() {
   const customers = await fetchCustomers();
@@ -17,7 +17,7 @@ export default async function Page() {
           },
         ]}
       />
-      <Form customers={customers} />
+      <InvoiceForm customers={customers} />
     </main>
   );
 }

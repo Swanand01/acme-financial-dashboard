644f3d17423dbbc504ab38adcdc19ea31672954b
Swanand Mathekar <swanandmathekar@gmail.com>
2024-01-17 00:09:45 +0530

    Refactor Login Page and update imports

--- app/users/login/page.tsx
+++ app/users/login/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
+import LoginForm from '@/app/ui/users/login/form';
 
-export default function LoginPage() {
+export default function Page() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
